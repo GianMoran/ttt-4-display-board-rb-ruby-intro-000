2f260ae08adae8e46a3a22c483ccc30cfe56c40d
Gian Moran <gpmoran_@outlook.com>
2019-12-29 04:56:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,4 +10,11 @@ def display_board(board)
 end
 if board[4]
   board[4] = "X"
+  puts " #{board[0]}  | #{board[1]} | #{board[2]}  "
+  puts "-------------------------------------------"
+  puts " #{board[3]}  |#{board[4]}  | #{board[5]}  "
+  puts "-------------------------------------------"
+  puts " #{board[6]}  | #{board[7]} | #{board[8]}  "
 end 
+end
+end
